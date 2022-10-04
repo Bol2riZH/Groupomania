171ef781fd819eed2ef9915120bef0b69a6b6a01
Matthieu <reungoat.matthieu@gmail.com>
2022-10-04 09:11:43 +0200

    add logout function

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,25 @@
-import React from 'react';
-
-import StorageComponent from '../hooks/useStorage/StorageComponent';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
-  const user = localStorage.getItem('user');
+  const userLog = JSON.parse(localStorage.getItem('user'));
+  const [log, setLog] = useState(userLog?.token);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (log === '')
+      setTimeout(() => {
+        navigate('/');
+      }, 600);
+  }, [log]);
 
   return (
     <div>
-      <StorageComponent>hello</StorageComponent>
-      <h1 style={{ color: '#eec5b0' }}>{user}</h1>
+      <h1 style={{ color: '#eec5b0' }}>HOME</h1>
       <button
         onClick={() => {
           localStorage.removeItem('user');
+          setLog('');
         }}
       >
         log out
